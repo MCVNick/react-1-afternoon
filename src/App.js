86f98a78cc5d236fddebd9a8b6e086e454a02966
MCVNick <mcvnick@gmail.com>
2019-01-13 15:57:47 -0700

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import TopicBrowser from './components/TopicBrowser/TopicBrowser'
 import EvenAndOdd from './components/Topics/EvenAndOdd'
 import FilterObject from './components/Topics/FilterObject'
 import FilterString from './components/Topics/FilterString'
